63bf9eb3b66864188fd0740fe663df957e53f09f
Kay <ky9@ornl.gov>
2022-05-12 17:07:31 -0400

    Initial import

---
+++ src/main/java/CompiledCode.java
@@ -0,0 +1,41 @@
+
+/** Example for "manually" editing source code, compiling and running it.
+ * 
+ *  On Windows:
+ *  <pre>
+ *  cd git\Summer2022\src\main\java
+ *  notepad CompiledCode.java 
+ *  \Users\Public\wpilib\2022\jdk\bin\javac CompiledCode.java
+ *  notepad CompiledCode.class
+ *  \Users\Public\wpilib\2022\jdk\bin\javap -v -p -c CompiledCode.class
+ *  \Users\Public\wpilib\2022\jdk\bin\java CompiledCode
+ *  </pre>
+ *  
+ *  On Mac:
+ *  <pre>
+ *  cd ~/git/Summer2022/src/main/java
+ *  open -a TextEdit CompiledCode.java 
+ *  ~/wpilib/2022/jdk/bin/javac CompiledCode.java 
+ *  open -a TextEdit CompiledCode.class
+ *  ~/wpilib/2022/jdk/bin/javap -v -p -c CompiledCode.class
+ *  ~/wpilib/2022/jdk/bin/java CompiledCode
+ *  </pre>
+ * 
+ *  Compare that to editing the code in VSCode and pressing "Run"
+ */
+public class CompiledCode
+{
+    public static void main(String[] args)
+    {
+        // Circle with radius ...
+        double r = 27.5;
+        double pi = 3.1415;
+        
+        double circ = 2*pi*r;
+        double area = pi*r*r;
+
+        System.out.println("For a circle with radius " + r + ",");
+        System.out.println("the circumference is " + circ);
+        System.out.println("and the area is " + area);
+    }    
+}
---
+++ src/main/java/frc/robot/SwervyRobot.java
@@ -0,0 +1,21 @@
+// Copyright (c) FIRST Team 2393 and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+/** Robot for testing swerve drive */
+public class SwervyRobot extends TimedRobot
+{
+    @Override
+    public void robotInit()
+    {
+        System.out.println("********************************");
+        System.out.println("********************************");
+        System.out.println("** " + getClass().getName());
+        System.out.println("********************************");
+        System.out.println("********************************");
+    }
+}
