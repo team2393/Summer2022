eb08b2077bf8d2b9efc3f0960c36c638181c7080
Kay <kay.kasemir@gmail.com>
2022-05-13 06:33:48 -0400

    Format

--- src/main/java/CompiledCode.java
+++ src/main/java/CompiledCode.java
@@ -1,27 +1,27 @@
 
 /** Example for "manually" editing source code, compiling and running it.
  * 
- *  On Windows:
+ *  On Windows, open a "command prompt" and try this:
  *  <pre>
- *  cd git\Summer2022\src\main\java
- *  notepad CompiledCode.java 
- *  \Users\Public\wpilib\2022\jdk\bin\javac CompiledCode.java
- *  notepad CompiledCode.class
- *  \Users\Public\wpilib\2022\jdk\bin\javap -v -p -c CompiledCode.class
- *  \Users\Public\wpilib\2022\jdk\bin\java CompiledCode
+ *    cd git\Summer2022\src\main\java
+ *    notepad CompiledCode.java 
+ *    \Users\Public\wpilib\2022\jdk\bin\javac CompiledCode.java
+ *    notepad CompiledCode.class
+ *    \Users\Public\wpilib\2022\jdk\bin\javap -v -p -c CompiledCode.class
+ *    \Users\Public\wpilib\2022\jdk\bin\java CompiledCode
  *  </pre>
  *  
- *  On Mac:
+ *  On Mac, open a "Terminal" and try this:
  *  <pre>
- *  cd ~/git/Summer2022/src/main/java
- *  open -a TextEdit CompiledCode.java 
- *  ~/wpilib/2022/jdk/bin/javac CompiledCode.java 
- *  open -a TextEdit CompiledCode.class
- *  ~/wpilib/2022/jdk/bin/javap -v -p -c CompiledCode.class
- *  ~/wpilib/2022/jdk/bin/java CompiledCode
+ *    cd git/Summer2022/src/main/java
+ *    open -a TextEdit CompiledCode.java 
+ *    ~/wpilib/2022/jdk/bin/javac CompiledCode.java 
+ *    open -a TextEdit CompiledCode.class
+ *    ~/wpilib/2022/jdk/bin/javap -v -p -c CompiledCode.class
+ *    ~/wpilib/2022/jdk/bin/java CompiledCode
  *  </pre>
  * 
- *  Compare that to editing the code in VSCode and pressing "Run"
+ *  Compare that to editing this file in VSCode and pressing "Run"
  */
 public class CompiledCode
 {
