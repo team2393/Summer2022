a494a642b9f1353d7d957da9b572c7f485f65e16
Kay <kay.kasemir@gmail.com>
2022-06-13 15:43:47 -0400

    Array example

---
+++ src/main/java/tutorial/Arrays.java
@@ -0,0 +1,41 @@
+package tutorial;
+
+import tutorial.person.Person;
+
+public class Arrays
+{
+    public static void main(String[] args)
+    {
+        Person one_person = new Person("Fred", 21);
+        
+        // Arrays keep more than one thing of a type
+        Person[] the_smiths = new Person[4];
+        // Creating a new Whatever[count] array creates just the array,
+        // need to then set the [elements]:
+        the_smiths[0] = new Person("Dad", 47);
+        the_smiths[1] = new Person("Mum", 48);
+        the_smiths[2] = new Person("Daughter", 17);
+        the_smiths[3] = new Person("Son", 14);
+
+        // May also combine creating the array and setting the elements:
+        Person[] the_millers = new Person[]
+        {
+            new Person("Grandpa", 78),
+            new Person("Grandma", 75),
+            new Person("Husband", 52),
+            new Person("Wife", 50),
+            new Person("Son", 21)
+        };
+        
+        // Can access specfic element:
+        System.out.println(the_millers[1].name);
+
+        // Loop over all elements by index
+        for (int i=0; i<the_smiths.length; ++i)
+            System.out.println(the_smiths[i].name);
+
+        // If index doesn't matter, this is the shortest way:
+        for (Person person : the_millers)
+            System.out.println(person.name);
+    }
+}
