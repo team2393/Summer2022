3abfa01510831e338091d426136f94562c161436
Kay <kay.kasemir@gmail.com>
2022-06-13 21:22:51 -0400

    Array example

--- src/main/java/tutorial/Arrays.java
+++ src/main/java/tutorial/Arrays.java
@@ -7,6 +7,7 @@ public class Arrays
     public static void main(String[] args)
     {
         Person one_person = new Person("Fred", 21);
+        System.out.println(one_person.name);
         
         // Arrays keep more than one thing of a type
         Person[] the_smiths = new Person[4];
@@ -27,14 +28,14 @@ public class Arrays
             new Person("Son", 21)
         };
         
-        // Can access specfic element:
+        // Access specfic element
         System.out.println(the_millers[1].name);
 
         // Loop over all elements by index
         for (int i=0; i<the_smiths.length; ++i)
-            System.out.println(the_smiths[i].name);
+            System.out.println("Element " + i + " is " + the_smiths[i].name);
 
-        // If index doesn't matter, this is the shortest way:
+        // If index doesn't matter, this is shorter
         for (Person person : the_millers)
             System.out.println(person.name);
     }
